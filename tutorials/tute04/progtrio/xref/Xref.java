6b013dc2b1ad9240e21d9584b30c72ab17a12295
quanganh2001 <89558595+quanganh2001@users.noreply.github.com>
2023-06-06 21:37:59 +0700

    Xref initial design

---
+++ tutorials/tute04/progtrio/xref/Xref.java
@@ -0,0 +1,17 @@
+/* Xref.java */
+package tute04.progtrio.xref;
+
+import java.util.List;
+import java.util.Map;
+
+import utils.NotPossibleException;
+
+public class Xref {
+	public static void main(String[] args) {
+		
+	}
+	
+	Map<Word, List<Integer>> indexDocument(String doc) throws NotPossibleException {
+		
+	}
+}
