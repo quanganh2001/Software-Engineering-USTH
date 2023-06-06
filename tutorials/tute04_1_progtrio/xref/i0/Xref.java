008af7d64d155efb0a4e1d30549208255f841376
quanganh2001 <89558595+quanganh2001@users.noreply.github.com>
2023-06-06 21:50:40 +0700

    Xref initial class and design

---
+++ tutorials/tute04_1_progtrio/xref/i0/Xref.java
@@ -0,0 +1,33 @@
+package tute04_1_progtrio.xref.i0;
+
+import java.util.List;
+import java.util.Map;
+
+import utils.NotPossibleException;
+
+/**
+ * @overview Represents a program that index a document to determine the lines in which each word appears
+ *
+ * @author Duc Minh Le (ducmle)
+ * 
+ * @version
+ */
+public class Xref {
+	/**
+	 * @effects starts an Xref running
+	 */
+	public static void main(String[] args) {
+		
+	}
+	
+	/**
+	 * @effects
+	 * 	if doc is not a text document
+	 * 		throws NotPossbileException
+	 * 	else
+	 * 		for each word containing more than one letter,
+	 * 		record the lines in which it appears (e.g. complier: 3, 17, 25)/
+	 * 		Return the result.
+	 */
+	Map<Word, List<Integer>> indexDocument(String doc) throws NotPossibleException;
+}
