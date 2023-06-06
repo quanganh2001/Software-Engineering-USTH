58afeccd36e8f36160d7ad2f4fc7a5a35bc99935
quanganh2001 <89558595+quanganh2001@users.noreply.github.com>
2023-06-06 21:37:27 +0700

    Word java initial design

---
+++ tutorials/tute04/progtrio/xref/Word.java
@@ -0,0 +1,12 @@
+/* Word.java */
+package tute04.progtrio.xref;
+
+import utils.NotPossibleException;
+
+public class Word {
+	private String label;
+	
+	public Word(String content) throws NotPossibleException {
+		
+	}
+}
