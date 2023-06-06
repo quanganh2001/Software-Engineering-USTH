a45f634dc1ce21f405d1c5e52349354fd0f18cce
quanganh2001 <89558595+quanganh2001@users.noreply.github.com>
2023-06-06 21:36:30 +0700

    Tutorial 04: Document initial desgin

---
+++ tutorials/tute04/progtrio/xref/Document.java
@@ -0,0 +1,23 @@
+/* Document.java */
+package tute04.progtrio.xref;
+
+import java.util.List;
+import java.util.Map;
+
+import utils.NotPossibleException;
+
+public class Document {
+	private List<Line> lines;
+	
+	private String doc;
+	
+	private Map<Word, List<Line>> wordLocations;
+	
+	public Document(String doc) throws NotPossibleException {
+		
+	}
+	
+	public Map<Word, List<Line>> wordLocations() {
+		
+	}
+}
